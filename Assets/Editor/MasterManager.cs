049aa20a0404084714ab2f13a572e29b5064d9da
ETU\a.gargasson <a.gargasson@rubika-edu.com>
2017-04-20 19:24:34 +0200

    first commit

---
+++ Assets/Editor/MasterManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+[ExecuteInEditMode]
+class MasterManager : EditorWindow
+{
+    [MenuItem("Basic Manager/Master Manager")]
+
+    public static void ShowWindow()
+    {
+        EditorWindow.GetWindow(typeof(MasterManager));
+    }
+
+    void OnGUI()
+    {
+        if (GUILayout.Button("Create Game Manager"))
+        {
+            GameObject go = new GameObject("Game Manager");
+            go.tag = "GameManager";
+            go.AddComponent<GameManager>();
+        }
+    }
+}
